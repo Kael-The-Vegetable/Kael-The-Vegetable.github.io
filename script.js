a62d62dcd68712a269c609e7e6630f76bd20d915
HAL9000-GH <cody_kostiuk@yahoo.ca>
2025-05-18 20:58:01 -0600

    No caching on XML fetch requests.

--- script.js
+++ script.js
@@ -20,7 +20,13 @@ function loadContent(page) {
     content.style.transition = 'opacity 0.5s ease'; // prepare for next fade
     // await sleep(1000); // (*) testing delay
 
-    fetch(`${page}.xml`) // fetch xml file
+    fetch(`${page}.xml?timestamp=${new Date().getTime()}`, { // (!) unique url request to avoid caching
+        headers: { // (!) additional no-caching parameters
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0"
+        }
+    }) // fetch xml file
         .then(response => {
             if (!response.ok) {
                 throw new Error('Content not found!');
