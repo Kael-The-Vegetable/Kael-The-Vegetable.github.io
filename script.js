e78cd447e1b99debc97e2765a9b52286c298854f
HAL9000-GH <cody_kostiuk@yahoo.ca>
2025-05-18 21:07:09 -0600

    Reverting no-caching code.

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function loadContent(page) {
     content.style.transition = 'opacity 0.5s ease'; // prepare for next fade
     // await sleep(1000); // (*) testing delay
 
+    /*
     fetch(`${page}.xml?timestamp=${new Date().getTime()}`, { // (!) unique url request to avoid caching
         headers: { // (!) additional no-caching parameters
             "Cache-Control": "no-cache, no-store, must-revalidate",
@@ -27,6 +28,8 @@ function loadContent(page) {
             "Expires": "0"
         }
     }) // fetch xml file
+    */
+   fetch(`${page}.xml`) // fetch xml file
         .then(response => {
             if (!response.ok) {
                 throw new Error('Content not found!');
