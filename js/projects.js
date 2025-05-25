1335be34ff133fca6306746aacc5e67382856bc2
Kael-The-Vegetable <kkostiuk3@nait.ca>
2025-05-25 10:06:15 -0600

    ♻️ Altered Strings to be short with additional spaces and colons built after the fact.

--- js/projects.js
+++ js/projects.js
@@ -2,16 +2,16 @@
 // TODO: go through adjectives, themes, genres, and sequels for any potential wrong wordings and rewrite.
 export class Projects {
     static #adjective = [
-      "Ancient ", "Angry ", "Atomic ", "Awkward ", "Bizarre ", "Blazing ", "Bloody ", "Broken ", "Brutal ","Chaotic ", "Clever ", "Cold ", "Cosmic ", "Creepy ", "Cursed ", "Dark ", "Deadly ", "Deranged ", "Doomed ", "Draconic ", "Dusty ", "Electric ", "Enraged ", "Eternal ", "Filthy ", "Freaky ", "Frosty ", "Galactic ", "Greedy ", "Grumpy ", "Haunted ", "Heroic ", "Hollow ", "Hot ", "Ironic ", "Juicy ", "Lonely ", "Lucky ", "Lunar ", "Mighty ", "Nuclear ", "Radiant ", "Rusty ", "Salty ", "Savage ", "Sassy ", "Slimy ", "Spooky ", "Vicious "
+      "Ancient", "Angry", "Atomic", "Awkward", "Bizarre", "Blazing", "Bloody", "Broken", "Brutal","Chaotic", "Clever", "Cold", "Cosmic", "Creepy", "Cursed", "Dark", "Deadly", "Deranged", "Doomed", "Draconic", "Dusty", "Electric", "Enraged", "Eternal", "Filthy", "Freaky", "Frosty", "Galactic", "Greedy", "Grumpy", "Haunted", "Heroic", "Hollow", "Hot", "Ironic", "Juicy", "Lonely", "Lucky", "Lunar", "Mighty", "Nuclear ", "Radiant", "Rusty", "Salty", "Savage", "Sassy", "Slimy", "Spooky", "Vicious"
     ]; // 50 adjectives
     static #theme = [ 
-      "Academic ", "Alien ", "Apocalypse ", "Arctic ", "Baking ", "Castle ", "Caveman ", "City ", "Cyberpunk ", "Deep Sea ", "Desert ", "Digital ", "Dungeoning ", "Dystopia ", "Forest ", "Galactic ", "Garage ", "Graveyard ", "Hacking ", "Hell ", "High School ", "Hospital ", "Iceberg ", "Island ", "Jungle ", "Kingdom ", "Laboratory ", "Library ", "Mars ", "Medieval ", "Metro ", "Mining ", "Moon ", "Museum ", "Office ", "Outer Space ", "Prison ", "Robotic ", "Ruins ", "Space Station ", "Suburb ", "Swamp ", "Temple ", "Time Traveling ", "Toy Store ", "Train ", "Underworld ", "Village ", "Wasteland ", "Wild West "
+      "Academic", "Alien", "Apocalypse", "Arctic", "Baking", "Castle", "Caveman", "City", "Cyberpunk", "Deep Sea", "Desert", "Digital", "Dungeoning", "Dystopia", "Forest", "Galactic", "Garage", "Graveyard", "Hacking", "Hell", "High School", "Hospital", "Iceberg", "Island", "Jungle", "Kingdom", "Laboratory", "Library", "Mars", "Medieval", "Metro", "Mining", "Moon", "Museum", "Office", "Outer Space", "Prison", "Robotic", "Ruins", "Space Station", "Suburb", "Swamp", "Temple", "Time Traveling", "Toy Store", "Train", "Underworld", "Village", "Wasteland", "Wild West"
     ]; // 50 themes
     static #genre = [
       "Adventure", "Arena", "Auto Clash", "Battle Royale", "Brawl", "Builder", "Card Clash", "Clicker", "Combat", "Cooking", "Dates", "Deckbuilder", "Dungeons", "Escape Room", "Farming", "Fighting", "Fishing", "Flight", "FPS", "God", "Hack-and-Slash", "Horror", "Idle", "Infiltration", "Interactive", "JRPG", "Jumps", "Life", "Management", "Mania", "Match-3", "Mazes", "MMO", "MOBA", "Music", "Mysteries", "Partying", "Point & Click", "Race", "Rhythm", "Roguelike",  "Sandbox", "Shooter", "Simulator", "Strategy", "Synthcraft", "Tinkerlabs", "Tower", "World"
     ]; // 50 genres
     static #sequelTag = [
-      "", ": <em>Aftermath</em>", ": <em>Armageddon</em>", ": <em>Back in Action</em>", ": <em>Code Orange</em>", ": <em>Director's Cut</em>", ": <em>Dominion</em>", ": <em>Extended Edition</em>", ": <em>Final Chapter</em>", ": <em>Intercepted</em>", ": <em>Last Stand</em>", ": <em>Origins</em>", ": <em>Prologue</em>", ": <em>Rebirth</em>", ": <em>Reloaded</em>", ": <em>Remastered?</em>", ": <em>Resurrection</em>", ": <em>Returns</em>", ": <em>Redux</em>", ": <em>Revolution</em>", ": <em>The Awakening</em>", ": <em>The Fall</em>", ": <em>The Better One</em>", ": <em>The Lost Chapters</em>", ": <em>The Reckoning</em>", ": <em>X</em>"
+      "Aftermath", "Armageddon", "Back in Action", "Code Orange", "Director's Cut", "Dominion", "Extended Edition", "Final Chapter", "Intercepted", "Last Stand", "Origins", "Prologue", "Rebirth", "Reloaded", "Remastered?", "Resurrection", "Returns", "Redux", "Revolution", "The Awakening", "The Fall", "The Better One", ":The Lost Chapters", "The Reckoning", "X"
     ]; // 26 sequel tags including 1 empty
 
     // Constants for internal calculations
@@ -20,7 +20,7 @@ export class Projects {
     static #GENRE_LEN = Projects.#genre.length;
     static #SEQUEL_LEN = Projects.#sequelTag.length;
 
-    static TOTAL_COMBOS = Projects.#ADJECTIVE_LEN * Projects.#THEME_LEN * Projects.#GENRE_LEN * (Projects.#SEQUEL_LEN);
+    static TOTAL_COMBOS = Projects.#ADJECTIVE_LEN * Projects.#THEME_LEN * Projects.#GENRE_LEN * (Projects.#SEQUEL_LEN + 1);
     static #inverseTotal = 1 / this.TOTAL_COMBOS;
 
     static TOTAL_NO_SEQUELS = Projects.#ADJECTIVE_LEN * Projects.#THEME_LEN * Projects.#GENRE_LEN;
@@ -83,10 +83,10 @@ export class Projects {
         const adjectiveIndex = index;
 
         return (
-            Projects.#adjective[adjectiveIndex] +
-            Projects.#theme[themeIndex] +
+            Projects.#adjective[adjectiveIndex] + " " +
+            Projects.#theme[themeIndex] + " " +
             Projects.#genre[genreIndex] +
-            (sequelIndex !== -1 ? Projects.#sequelTag[sequelIndex] : "")
+            (sequelIndex !== -1 ? ": <em>" + Projects.#sequelTag[sequelIndex] + "</em>" : "")
         );
     }
     static clearUsed() {
